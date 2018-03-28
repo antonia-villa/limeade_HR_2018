1c60c8fb035df344be39613df1676cd9cde5b254
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-03-27 17:10:06 -0700

    updated code from codepen

---
+++ script.js
@@ -0,0 +1,167 @@
+var data = [{
+	"name": "Shadow",
+	"type": "Cat",
+	"breed": "Shorthair",
+	"gender": "F",
+	"description": "Grey cat with sass",
+	"location": "Issaquah, WA",
+	"properties": {
+		"price": 25.50,
+		"is_vaccinated": true,
+		"birth_date": "2012-02-23",
+		"attributes": {
+			"fur": "short",
+			"color": "grey",
+			"eyes": "green"
+		}
+	},
+	"tags": ["kid friendly", "quiet", "shy"]
+}, {
+	"name": "Rex",
+	"type": "Dog",
+	"breed": "Boxer",
+	"gender": "M",
+	"description": "Puppy with potential",
+	"location": "Seattle, WA",
+	"properties": {
+		"price": 200,
+		"is_vaccinated": true,
+		"birth_date": "2017-01-23T12:00:00.000Z",
+		"attributes": {
+			"fur": "short",
+			"color": "orange",
+			"eyes": "green"
+		}
+	},
+	"tags": ["puppy", "affectionate", "kid friendly"]
+}, {
+	"name": "Tiger",
+	"type": "Cat",
+	"breed": "Tabby",
+	"gender": "F",
+	"description": "Older cat with patience",
+	"location": "Seattle, WA",
+	"properties": {
+		"price": 25,
+		"is_vaccinated": true,
+		"birth_date": "2011-05-13T12:00:00.000Z",
+		"attributes": {
+			"fur": "long",
+			"color": "orange",
+			"eyes": "green"
+		}
+	},
+	"tags": ["affectionate", "kid friendly"]
+}, {
+	"name": "Joe",
+	"type": "Cat",
+	"breed": "Siamese",
+	"gender": "M",
+	"description": "Kitten with voice",
+	"location": "Bellevue, WA",
+	"properties": {
+		"price": 135,
+		"is_vaccinated": true,
+		"birth_date": "2017-01-11T12:00:00.000Z",
+		"attributes": {
+			"fur": "short",
+			"color": "white",
+			"eyes": "grey"
+		}
+	},
+	"tags": ["vocal", "ornery"]
+}, {
+	"name": "Josie",
+	"type": "Cat",
+	"breed": "Tabby",
+	"gender": "F",
+	"description": "Kitten",
+	"location": "Bellevue, WA",
+	"properties": {
+		"price": 80,
+		"is_vaccinated": false,
+		"birth_date": "2017-01-31T12:00:00.000Z",
+		"attributes": {
+			"fur": "short",
+			"color": "black",
+			"eyes": "blue"
+		}
+	},
+	"tags": ["quiet", "shy"]
+}, {
+	"name": "Joseph",
+	"type": "Dog",
+	"breed": "Welsh Corgi",
+	"gender": "M",
+	"description": "Loyal Corgi",
+	"location": "Bellevue, WA",
+	"properties": {
+		"price": 280,
+		"is_vaccinated": true,
+		"birth_date": "2012-08-10T12:00:00.000Z",
+		"attributes": {
+			"fur": "medium",
+			"color": "orange and white",
+			"eyes": "brown"
+		}
+	},
+	"tags": ["lap dog", "vocal"]
+}, {
+	"name": "Jor",
+	"type": "Dog",
+	"breed": "German Shepherd",
+	"gender": "M",
+	"description": "Puppy that needs training",
+	"location": "Bothell, WA",
+	"properties": {
+		"price": 320,
+		"is_vaccinated": true,
+		"birth_date": "2016-12-10T12:00:00.000Z",
+		"attributes": {
+			"fur": "medium",
+			"color": "black",
+			"eyes": "brown"
+		}
+	},
+	"tags": ["chews", "active", "kid friendly"]
+}, {
+	"name": "Jon Jon",
+	"type": "Cat",
+	"breed": "Tabby",
+	"gender": "M",
+	"description": "Older lap cat",
+	"location": "Kirkland, WA",
+	"properties": {
+		"price": 22,
+		"is_vaccinated": true,
+		"birth_date": "2009-11-10T12:00:00.000Z",
+		"attributes": {
+			"fur": "short",
+			"color": "black and white",
+			"eyes": "green"
+		}
+	},
+	"tags": ["sleeps", "quiet", "kid friendly"]
+}, {
+	"name": "Janie",
+	"type": "Dog",
+	"breed": "Poodle",
+	"gender": "F",
+	"description": "Well trained companion",
+	"location": "Kirkland, WA",
+	"properties": {
+		"price": 220,
+		"is_vaccinated": true,
+		"birth_date": "2015-11-10T12:00:00.000Z",
+		"attributes": {
+			"fur": "short",
+			"color": "white",
+			"eyes": "black"
+		}
+	},
+	"tags": ["trained", "obedient", "kid friendly"]
+}]
+
+var myPets = {};
+myPets.pets = data;
+
